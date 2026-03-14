8c7139588975eaded0fc5964333a9787e8da776c
RKNITH <raviranjankofficial@gmail.com>
2026-03-14 16:01:37 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ import axios from "axios";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST"],
+  }
+));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
